547c2e36d9abfff1f69bd5f66472ef2cea575d5a
lemonkns <lemonkns@gmail.com>
2025-09-18 20:00:14 +0700

    fix ex13

--- cell05/ex13/append_it.py
+++ cell05/ex13/append_it.py
@@ -5,8 +5,8 @@ output = []
 for arg in args:
     if not arg.endswith("ism"):
         output.append(arg + "ism")
-    if not output:
-        print("none")
-    else:
-        for item in output:
-            print(item)
+if not output:
+    print("none")
+else:
+    for item in output:
+        print(item)
